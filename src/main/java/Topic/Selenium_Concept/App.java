c9c900eaeecbb6d79987cbb1e50f0f3d08be6f62
GPK2105 <praveen21gpk@gmail.com>
2024-09-12 21:53:16 +0530

    first commit

---
+++ src/main/java/Topic/Selenium_Concept/App.java
@@ -0,0 +1,10 @@
+package Topic.Selenium_Concept;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Topic/Selenium_Concept/testlocators.java
@@ -0,0 +1,15 @@
+package Topic.Selenium_Concept;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testlocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
