eb357a324e82194243171988316d8a4f77e6f092
GPK2105 <praveen21gpk@gmail.com>
2024-09-12 22:05:43 +0530

    locators

--- src/test/java/Topic/Selenium_Concept/locators.java
+++ src/test/java/Topic/Selenium_Concept/locators.java
@@ -3,12 +3,13 @@ package Topic.Selenium_Concept;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class testlocators {
+public class locators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/");
 		
 	}
 
